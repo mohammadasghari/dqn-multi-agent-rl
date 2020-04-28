1381c9e22fcf2115ca85647e533f2ca535ecf0be
INDHRA KIRANU N A <55424779+indhra007@users.noreply.github.com>
2020-04-28 16:06:15 +0530

    Update and rename brain_n_agents.py to brain.py

--- brain.py
+++ brain.py
@@ -4,7 +4,7 @@ Created on Wednesday Jan  16 2019
 @author: Seyed Mohammad Asghari
 @github: https://github.com/s3yyy3d-m
 """
-# Brain for n agents
+
 from past.builtins import xrange
 import os
 from keras.models import Sequential, Model
